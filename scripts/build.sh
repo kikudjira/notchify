175b84b23ed01f5b15989d5652f56faee831c618
Andrey Sekirkin <andreysek@gmail.com>
2026-04-19 17:42:58 +0200

    feat(animation): refresh all 5 mascot animations → v1.0.27
    Updated sprites: cm_start (7f), cm_working (3f), cm_waiting (8f),
    cm_done (4f), cm_bye (7f). Removed obsolete bye_07.png frame.

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,9 +38,9 @@ cat > "$CONTENTS/Info.plist" << 'PLIST'
     <key>CFBundleName</key>
     <string>Notchify</string>
     <key>CFBundleVersion</key>
-    <string>1.0.26</string>
+    <string>1.0.27</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0.26</string>
+    <string>1.0.27</string>
     <key>LSUIElement</key>
     <true/>
     <key>NSPrincipalClass</key>
